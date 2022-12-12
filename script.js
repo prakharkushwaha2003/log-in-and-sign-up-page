dccaf5b85da1c9433706b71be2549f45695b23fa
prakharkushwaha2003 <prakhar.kushwaha@eminds.ai>
2022-12-12 16:53:27 +0530

    login and sign up page

---
+++ script.js
@@ -0,0 +1,20 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+var a = document.getElementById("main-content");
+
+
+
+function register() {
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+    a.style.backgroundColor = "PaleGreen"
+   
+}
+function login() {
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+    a.style.backgroundColor = "white"
+}
